d8139fd3d1eddb2f0cfc7eefbfa319e8566b2668
RobOrtiz <mrrobortiz@gmail.com>
2021-01-10 22:34:13 -0800

    added controller

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ var PORT = process.env.PORT || 8080;
 
 // Create express app instance.
 var app = express();
+
+app.use(express.static("public"));
+
+// parse
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Routes
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App is listening at localhost: " + PORT);
+});
