10b35e0e7b07ba1cee567f19dbc3e21b14b6edab
RobOrtiz <mrrobortiz@gmail.com>
2021-01-10 17:32:12 -0800

    Added orm js file with code

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INTEGER NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
-	devoured BOOLEAN NOT NULL,
+	devoured BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY (id)
 );
 
