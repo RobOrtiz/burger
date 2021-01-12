327736b2bd65c5cf58f6dca6f06da53593651aa0
RobOrtiz <mrrobortiz@gmail.com>
2021-01-11 23:31:37 -0800

    added delete to orm file

--- db/config/orm.js
+++ db/config/orm.js
@@ -36,6 +36,20 @@ var orm = {
             if (err) throw err;
             cb(result);
         });
+    },
+    deleteOne: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
     }
 };
 
