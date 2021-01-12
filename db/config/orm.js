82d34c038c91108b397d89ea5e172b1e41722c09
RobOrtiz <mrrobortiz@gmail.com>
2021-01-12 14:36:20 -0800

    updated css

--- db/config/orm.js
+++ db/config/orm.js
@@ -36,20 +36,6 @@ var orm = {
             if (err) throw err;
             cb(result);
         });
-    },
-    deleteOne: function(table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
-        queryString += " WHERE ";
-        queryString += condition;
-
-        console.log(queryString);
-
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err
-            }
-            cb(result);
-        });
     }
 };
 
